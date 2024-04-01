9c6535ec41cd83fad9786bad21115c3d4a0dab59
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-04-01 18:13:51 +0900

    컴포넌트 Tick 제거

--- Source/WOS/Gizmo/MapLayoutViewer.h
+++ Source/WOS/Gizmo/MapLayoutViewer.h
@@ -18,16 +18,5 @@ public:
 	UPROPERTY(EditAnywhere)
 	bool showLayout = true;
 
-
 	UMapLayoutViewer();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-
 };
