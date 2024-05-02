59334ea1f11aee4267da18b34d63c8c4a83d4669
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-05-02 17:01:08 +0900

    블루프린트 참조

--- Source/WOS/Managers/NetObjectManager.cpp
+++ Source/WOS/Managers/NetObjectManager.cpp
@@ -5,16 +5,28 @@
 #include "GameFramework/Character.h"
 #include "GameActor/LocalPlayerCharacter.h"
 #include "Kismet/GameplayStatics.h"
+#include <UObject/ConstructorHelpers.h>
+
+UNetObjectManager::UNetObjectManager()
+{
+	static ConstructorHelpers::FClassFinder<ACharacter> PlayerBP(TEXT("/Script/Engine.Blueprint'/Game/Blueprints/GameActor/BP_LocalPlayerCharacter.BP_LocalPlayerCharacter_C'"));
+	if (PlayerBP.Succeeded())
+	{
+		PlayerClass = PlayerBP.Class;
+	}
+}
 
 void UNetObjectManager::HandleSpawnPlayer(uint64 ObjectId, FVector Position, FString Name, bool bIsMine) {
 	auto* World = GetWorld();
 	auto Rotation = FRotator(0, 0, 0);
-	APlayerCharacter* Player;
+	APlayerCharacter* Player = nullptr;
 	if (bIsMine) {
-		auto* Actor = World->SpawnActor(ALocalPlayerCharacter::StaticClass(), &Position, &Rotation);
+
+		auto* Actor = World->SpawnActor(PlayerClass, &Position, &Rotation);
+
 		Player = Cast<ALocalPlayerCharacter>(Actor);
 
-		auto Controller = UGameplayStatics::GetPlayerController(World, 0);
+		auto Controller = UGameplayStatics::GetPlayerController(World, 0); 
 		Player->Controller = Controller;
 	}
 	else {
