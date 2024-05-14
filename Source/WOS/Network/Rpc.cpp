c4b0b1a9d25090027681e385d3c513731fb7e27a
index <han93851207@gmail.com>
2024-05-14 14:50:22 +0900

    Rpc init

---
+++ Source/WOS/Network/Rpc.cpp
@@ -0,0 +1,16 @@
+﻿#include "Rpc.h"
+
+TMap<FString, RpcSystem::RpcProc> RpcSystem::RpcFuncTable;
+
+void RpcSystem::Register(FStringView FuncName, RpcProc ProcFunc)
+{
+	RpcFuncTable[FuncName.GetData()] = ProcFunc;
+}
+
+void RpcSystem::Execute(Rpc<> Rpc)
+{
+	FString FuncName;
+	Rpc >> FuncName;
+
+	RpcFuncTable[FuncName](Rpc);
+}
---
+++ Source/WOS/Network/Rpc.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "Packet.h"
+
+#define RPC(className, FuncName, ...)
+
+
+template<typename... Args>
+class Rpc : public Packet
+{
+public:
+	Rpc(Args&&... RpcParam)
+	{
+		std::string head = "RPC";
+		Data().resize(3);
+		std::copy(head.begin(), head.end()-1, Data().begin());
+
+		((*this << std::forward<Args>(RpcParam)), ...);
+	}
+	virtual void Read() override
+	{
+	}
+};
+
+
+class RpcSystem
+{
+	using RpcProc = TFunction<void(Rpc<>)>;
+public:
+	static void Register(FStringView FuncName, RpcProc ProcFunc);
+	static void Execute(Rpc<> Rpc);
+private:
+	static TMap<FString, RpcProc> RpcFuncTable;
+};
