be1f257a501250cea6980915adc6a57993b3ce89
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-05-20 20:49:34 +0900

    API 버전 반영

--- Source/WOS/Network/Handler/MMOPacketHandler.cpp
+++ Source/WOS/Network/Handler/MMOPacketHandler.cpp
@@ -10,12 +10,6 @@ bool mmo::PacketHandler::EnterGameResPacketHandler(TSharedPtr<Session> session, 
 	return false;
 }
 
-bool mmo::PacketHandler::LeaveMapPacketHandler(TSharedPtr<Session> session, TSharedPtr<NotifyLeaveMap> packet)
-{
-	UManager::Object()->HandleLeaveMap(packet.Get());
-	return false;
-}
-
 bool mmo::PacketHandler::EnterMapResPacketHandler(TSharedPtr<Session> session, TSharedPtr<EnterMapRes> packet)
 {
 	UManager::Get()->HandleEnterMap(packet.Get());
@@ -25,6 +19,12 @@ bool mmo::PacketHandler::EnterMapResPacketHandler(TSharedPtr<Session> session, T
 bool mmo::PacketHandler::SpawnPacketHandler(TSharedPtr<Session> session, TSharedPtr<Spawn> packet)
 {
 	UManager::Object()->HandleSpawnPlayer(packet.Get());
+	return false;
+}
+
+bool gen::mmo::PacketHandler::NotifyLeaveMapPacketHandler(TSharedPtr<Session> session, TSharedPtr<NotifyLeaveMap> packet)
+{
+	UManager::Object()->HandleLeaveMap(packet.Get());
 	return false;
 }
 
